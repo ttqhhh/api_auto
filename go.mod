1ceaff9383fea548937b5f338f9b941566752c2a
tangtianqing <tangtianqing2014@xiaochuankeji.cn>
2022-01-27 16:22:11 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,7 @@
+module api_auto
+
+go 1.16
+
+require github.com/beego/beego/v2 v2.0.1
+
+require github.com/smartystreets/goconvey v1.6.4
---
+++ routers/router.go
@@ -0,0 +1,10 @@
+package routers
+
+import (
+	"api_auto/controllers"
+	beego "github.com/beego/beego/v2/server/web"
+)
+
+func init() {
+    beego.Router("/", &controllers.MainController{})
+}
